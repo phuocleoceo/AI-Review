4871e184a4e53dd31cdc16360fa809ef1a4dd644
phuocleoceo <ht10082001@gmail.com>
2022-06-09 10:02:42 +0700

    Logistic Sklearn

---
+++ LogisticRegresison/code.py
@@ -0,0 +1,26 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+AB = np.array([[10, 1],
+               [5, 2],
+               [7, 1],
+               [4, 2.5],
+               [4, 1],
+               [5, 0.5],
+               [8, 0.5],
+               [8, 1],
+               [7, 0.3],
+               [6, 0.3]])
+C = np.array([1, 1, 1, 1, 0, 0, 0, 1, 0, 0])
+
+lr = LogisticRegression()
+lr.fit(AB, C)
+
+# pred = lr.predict(AB)
+# print(pred)
+# print(accuracy_score(C, pred))
+
+X_test = [[4, 3]]
+Y_pred = lr.predict(X_test)
+print(Y_pred[0])
